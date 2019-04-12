636abb2954a2956c00d61c4e7a647551c2e96dc3
yuminh54 <21800792@handong.edu>
2019-04-12 10:58:47 +0900

    sub2의 main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+	void sub1();
+        void sub2();
+        void sub3();
+        void sub4();
+
+int main(){
+	void sub2(){
+		printf("My name is YMH");
+	}
+
+
+	return 0;
+}

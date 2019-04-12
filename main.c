9af34a465b1933cfdb3a403b513d7cad3ffc23a6
yewewew <21800667@handong.edu>
2019-04-12 11:16:19 +0900

    정예원의 PR보냅니다!

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-	void sub1();
+	void sub1(){
+		printf("안녕하세요, 18학번 전산전자공학부 정예원입니다:)");
+	}
         void sub2();
         void sub3();
         void sub4();
 
 int main(){
+	sub1();
 	void sub2(){
 		printf("My name is YMH");
 	}
